cab8afcf6361149d5a3873d894626ad6f9859640
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-05-01 03:34:01 +0430

    Project cleaned: Archive codes removed from source. `tsconfig.json` also cleaned.

--- src/TextWrap.ts
+++ src/TextWrap.ts
@@ -41,7 +41,6 @@ export class TextWrap implements WrapStyle {
 	wrap(text: string, indents: string): WrapResult {
 		const markers: number[] = [];
 		const tabLength = this.tabLength;
-		const vLenBound = this.wrapOn + 1;
 		const length = text.length;
 		const indentsN = indents + this.continuationIndent;
 		const indentsNVLen = this.getVisualLength(indentsN);
@@ -52,7 +51,7 @@ export class TextWrap implements WrapStyle {
 		mainLoop: for (let i = 0, vLen = 0, vLen0 = 0, start = 0, marker = 0, c: string, fastCheck = false; i < length; ++i) {
 			c = text[i];
 			vLen += c === '\t' ? tabLength : 1;
-			//console.log(`A: ${c} / ${vLen}`);
+			//console.debug(`A: ${c} / ${vLen}`);
 			
 			searchBreakLine: do {
 				if (c === '\n') break searchBreakLine;
@@ -84,7 +83,7 @@ export class TextWrap implements WrapStyle {
 				
 				markers.push(marker);
 				wrappedText += text.slice(from, marker) + '\n' + indentsN;
-				//console.log(`L: [${from}, ${marker}) - ${vLen} - ${indentsNVLen + (vLen - vLen0)}\n${text.slice(from, marker)}`);
+				//console.debug(`L: [${from}, ${marker}) - ${vLen} - ${indentsNVLen + (vLen - vLen0)}\n${text.slice(from, marker)}`);
 				vLen = indentsNVLen + (vLen - vLen0);
 				vLen0 = indentsNVLen;
 				start = marker;	// Set to start of the next line
@@ -94,167 +93,24 @@ export class TextWrap implements WrapStyle {
 				
 			} while (false); // /searchBreakLine
 			
-			console.log('NN');
+			//console.debug('NN');
 			start = i;		// Set to start of the next line
 			vLen0 = 0;		// Reset vLen0
 			vLen = 0;		// Reset vLen
 		} // /mainLoop
 		
-		console.log(`OK:${from}\n${wrappedText}`);
+		//console.debug(`OK:${from}\n${wrappedText}`);
 		
 		return {
 			markers: markers,
 			wrappedText: wrappedText.slice(0, wrappedText.length - indentsN.length) + text.slice(from, length),  // CAVEAT: Don't omit `wrappedText.length` from the second argument of .slice()! Maybe: indentsN.length===0
 		}
 	}
-	
-	wrap0(text: string, indents: string): WrapResult0 {
-		//console.debug(JSON.stringify(text));
-		const lines: Line[] = [];
-		const markers: number[] = [];
-		let marker = 0;
-		let wrappedText = '';
-		
-		//console.debug(`wrapOn: ${this.wrapOn} - Received indents: ${JSON.stringify(indents)} - lengthN: ${lengthN}`);
-		const indentsN = indents + this.continuationIndent;
-		const indentsNVLen = this.getVisualLength(indentsN);
-		const lengthN = this.wrapOn - indentsNVLen;
-		
-		// 1. In zero-cycle loops this must NOT be `0`. See return statement.
-		// 2. In first-cycle of below loops this must be `1`.
-		let breakLineFound = 1;
-		
-		for (let i = 0; marker < text.length; ++i) {
-			let length: number;
-			let len: number;
-			let line: string;
-			let conditionalIndents: string;
-			
-			if (breakLineFound === 1) {
-				length = this.wrapOn;
-				conditionalIndents = '';
-			} else {
-				length = lengthN;
-				conditionalIndents = indentsN;
-			}
-			
-			len = this.reduceToVisualLength(text, length);  //console.debug(`Reduced-length (len): ${len}`);
-			line = text.substr(marker, length = len); //console.debug(`line0: ${JSON.stringify(line)}`);
-			// Store before-precess value of `len` to compare with its after-process value. See `process` label below.
-			
-			process:
-			{
-				let restText = text.slice(marker + len);
-				breakLineFound = 0;
-				len = line.indexOf('\n');
-				
-				if (len !== -1) {
-					//console.debug(`Found break-line at: ${len}`);
-					breakLineFound = 1;
-					break process;
-				}
-				
-				len = line.length;
-				
-				if (restText.length === 0) //{ console.debug(`Reached last line.`);
-					break process;				//}
-				
-				const precededWSesInRest = /^[^\S\xA0]+/.exec(restText);
-				//console.debug(`Found preceded white-spaces in the rest of the text: ${!!precededWSesInRest}\nrestText: ${JSON.stringify(restText)}`);
-				
-				if (precededWSesInRest !== null) {
-					const i = precededWSesInRest[0].indexOf('\n');
-					
-					if (i === -1) {
-						len += precededWSesInRest[0].length;
-						break process;
-					}
-					
-					//console.debug(`Found preceded break-line in \`restText\` at: ${i}`);
-					breakLineFound = 1;
-					len += i;
-					
-					break process;
-				}
-				
-				const latestWSToEnd = /[^\S\xA0][\S\xA0]*?$/.exec(line);
-				//console.debug(`Found latest white-space (+ subsequent characters to the end) in line: ${!!latestWSToEnd}\nline: ${JSON.stringify(line)}`);
-				
-				if (latestWSToEnd !== null) {
-					len = latestWSToEnd.index + 1;
-					
-					// The second condition will always be true if the first is true. Actually, it is the main condition we
-					// need to check, but we know in most cases (more than 95%) the first is true.
-					if (len > indentsNVLen || this.getVisualLength(line.slice(0, len)) > indentsNVLen) break process;
-					
-					console.info(`OOPS! A bad state! Wrapping is useless in this state. We have to continue. len: ${len}`);
-				}
-				
-				len = line.length;
-				
-				const firstWSesInRest = /[^\S\xA0]+/.exec(restText);
-				//console.debug(`Found first occurrence of white-spaces in the rest of the text: ${!!firstWSesInRest}\nrestText: ${JSON.stringify(restText)}`);
-				
-				if (firstWSesInRest === null) {
-					console.info(`Couldn't wrap. No white-space find!`);
-					len += restText.length;
-					break process;
-				}
-				
-				const i = firstWSesInRest[0].indexOf('\n');
-				
-				if (i === -1) {
-					len += firstWSesInRest.index + firstWSesInRest[0].length;
-					break process;
-				}
-				
-				//console.debug(`Found break-line in \`restText\` at: ${i}`);
-				breakLineFound = 1;
-				len += firstWSesInRest.index + i;
-				
-				//break process;
-			}
-			
-			line = conditionalIndents + (
-					len === length ? line : text.substr(marker, len)  // Caveat: Don't get new value of line by slicing previous value of it! Maybe: len > length
-			);
-			wrappedText += line + '\n';
-			marker += len + breakLineFound;
-			lines.push({
-				content: line,
-				endOffset: marker,
-				lineBreakAlreadyExists: breakLineFound === 1,
-			});
-			if (breakLineFound === 0) markers.push(marker);
-			
-			//console.log(`Wrapped at len: ${len}. marker: ${marker}, length: ${length}`);
-			//console.log(line);
-		}
-		
-		//console.debug(wrappedText);
-		return {
-			lines: lines,
-			markers: markers,
-			wrappedText: breakLineFound === 0 ? wrappedText.slice(0, -1) : wrappedText,
-		}
-	}
-}
-
-export interface WrapResult0 {
-	lines: Line[];
-	markers: number[];
-	wrappedText: string;
 }
 
 export interface WrapResult {
 	markers: number[];
 	wrappedText: string;
-}
-
-export interface Line {
-	content: string;
-	endOffset: number;
-	lineBreakAlreadyExists: boolean;
 }
 
 export interface WrapStyle {
