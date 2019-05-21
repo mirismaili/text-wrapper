7a818e47e792ae1ac55184bd04b5303e036c1393
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-05-21 19:12:24 +0430

    build(rollup): Outputs were separated per target (browser and node)
    Now, the project has 3 outputs:
    1. `main.umd.js`: Dependencies have not been bundled / Suitable to use in "node" and any other
        environment that is compatible with UMD module-system and can resolve dependencies.
    2. `bundle.umd.js`: Dependencies have been bundled / Suitable for browser environment and in
        <script> tag
    3. `bundle.esm.js`: Dependencies have been bundled / Suitable for browser environment but is
        compatible with ES6 imports.
    See: ".releaserc.json" file.

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@ import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import {terser} from "rollup-plugin-terser"
 import tempDir from 'temp-dir'
+import autoExternal from 'rollup-plugin-auto-external'
+import globals from 'rollup-plugin-node-globals'
+import builtins from 'rollup-plugin-node-builtins'
 
 const pkg = require('./package.json')
 
@@ -12,26 +15,49 @@ const libraryName = pkg.name.slice(pkg.name.indexOf('/') + 1)
 const libVarName = camelCase(libraryName)
 const libClassName = `${libVarName[0].toUpperCase() + libVarName.slice(1)}` // PascalCase
 
-// noinspection JSUnusedGlobalSymbols
-export default {
-	input: `src/${libClassName}.ts`,
+const input = `src/${libClassName}.ts`
+const watch = {include: 'src/**'}
+const isProd = process.env.BUILD === 'production'
+
+const commonPlugins = [
+	typescript({
+		useTsconfigDeclarationDir: true,
+		cacheRoot: `${tempDir}/.rpt2_cache`, // See: https://github.com/ezolenko/rollup-plugin-typescript2/issues/34#issuecomment-332591290
+	}),
+	commonjs(),
+	(isProd && terser()),
+	sourceMaps()
+]
+
+const bundledOutput = {
+	input: input,
 	output: [
-		{file: pkg.main, name: libVarName, format: 'umd', sourcemap: true, exports: 'named'},
-		{file: pkg.module, format: 'esm', sourcemap: true},
+		{
+			format: 'esm', file: pkg["bundle-module"], sourcemap: true
+		},
+		{
+			format: 'umd', file: pkg.bundle, name: libVarName, sourcemap: true, exports: 'named',
+		},
 	],
-	external: [],
-	watch: {
-		include: 'src/**',
-	},
-	plugins: [
-		// Compile TypeScript files.
-		typescript({
-			useTsconfigDeclarationDir: true,
-			cacheRoot: `${tempDir}/.rpt2_cache`, // See: https://github.com/ezolenko/rollup-plugin-typescript2/issues/34#issuecomment-332591290
-		}),
-		commonjs(),
+	watch: watch,
+	plugins: commonPlugins.concat([
+		globals(),
+		builtins(),
 		resolve(),
-		(process.env.BUILD === 'production' && terser()),
-		sourceMaps(),
-	],
+	]),
 }
+
+const dependentOutput = {
+	input: input,
+	output: {
+		format: 'umd', file: pkg.main, name: libVarName, sourcemap: true, exports: 'named',
+		globals: {}
+	},
+	watch: watch,
+	plugins: commonPlugins.concat([
+		autoExternal(),
+	]),
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default isProd ? [dependentOutput, bundledOutput] : dependentOutput
